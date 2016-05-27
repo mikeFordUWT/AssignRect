0576bbd38e072ee60befcfbbff2ca4a7d498da45
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-05-27 11:24:39 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+CANVAS_HEIGHT = 640;
+CANVAS_WIDTH = 800;
+
+function distance(a, b) {
+    var difX = a.x - b.x;
+    var difY = a.y - b.y;
+    return Math.sqrt(difX * difX + difY * difY);
+}
+
+function Background(game) {
+
+    this.x = 0;
+    this.y = 0;
+    Entity.call(this, game, 0, 0);
+    this.game = game;
+    this.ctx = game.ctx;
+}
+
+Background.prototype = new Entity();
+Background.prototype.constructor = Background;
+
+Background.prototype.update = function () {
+}
+
+Background.prototype.draw = function (ctx) {
+    ctx.beginPath();
+    ctx.fillStyle= "lightBlue";
+    ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fill();
+    ctx.closePath();
+    // Entity.prototype.draw.call(this);
+}
+
+
+var friction = 1;
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
+ASSET_MANAGER.queueDownload("./img/black.png");
+ASSET_MANAGER.queueDownload("./img/white.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    var canvas = document.getElementById('gameWorld');
+
+    var ctx = canvas.getContext('2d');
+    var gameEngine = new GameEngine();
+    var bg = new Background(gameEngine);
+    gameEngine.addEntity(bg);
+    var rect = new Rectangle(gameEngine, 0, 100, 400, 50);
+    var rect2 = new Rectangle(gameEngine,0, 300, 550, 50);
+    var rect3 = new Rectangle(gameEngine,200, 500, 400, 50);
+    var rect4 = new Rectangle(gameEngine, 500, 100, 400, 50);
+    var leader = new Circle(gameEngine, true);
+    leader.color = "white";
+
+
+    for(var i = 0; i < 2; i++){
+        var circle = new Circle(gameEngine);
+        gameEngine.addEntity(circle, false);
+    }
+    gameEngine.addEntity(leader);
+    // gameEngine.addEntity(circle);
+    gameEngine.addEntity(rect);
+    gameEngine.addEntity(rect3);
+    gameEngine.addEntity(rect2);
+    gameEngine.addEntity(rect4);
+
+
+
+
+
+
+
+
+    gameEngine.init(ctx);
+    gameEngine.start();
+});
